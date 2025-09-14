f66b68c3d0feb82722ed135886485c1b5baa0b2e
Andre Derme <andredermee@gmail.com>
2025-09-13 23:36:09 -0300

    Update home.ts

--- TCC/src/app/pages/home/home.ts
+++ TCC/src/app/pages/home/home.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
-export class Home {
+export class HomeComponent {
 
+  carros = [
+    {
+      nome: 'EcoSport',
+      ano: 2016,
+      descricao: 'O Ford EcoSport é um SUV compacto, conhecido por seu porte pequeno e design versátil. Ele oferece diversas opções de motorização, incluindo versões 1.5 e 2.0, com câmbio manual ou automático, e opções de tração dianteira ou integral.',
+      imagem: 'assets/ecosport.png'
+    },
+    {
+      nome: 'FOCUS',
+      ano: 2010,
+      descricao: 'O Ford Focus MK2, especialmente a sua reestilização MK2.5 lançada no Brasil em 2008, é um carro conhecido pela boa dirigibilidade, robustez e um pacote de equipamentos de destaque, principalmente nas versões mais completas como a Titanium. Oferecido com os motores Duratec 2.0 a gasolina (e depois Flex) e o Sigma 1.6 Flex.',
+      imagem: 'assets/focus.png'
+    },
+    {
+      nome: 'FIESTA SEDAN',
+      ano: 2016,
+      descricao: 'O Ford Fiesta Sedan 2016, ou New Fiesta Sedan 2016, é elogiado pelo bom desempenho do motor 1.6 16V flex, conforto e praticidade. A linha 2016 trouxe novidades como a versão SE Plus com mais airbags, central multimídia MyFord Touch com tela de 6,5” e câmera de ré nas versões mais caras.',
+      imagem: 'assets/Fiesta.png'
+    }
+  ];
+
+  
+  carroAtualIndex = 0;
+
+  constructor() { }
+
+  proximoCarro() {
+
+    this.carroAtualIndex = (this.carroAtualIndex + 1) % this.carros.length;
+  }
+
+  carroAnterior() {
+    if (this.carroAtualIndex === 0) {
+      this.carroAtualIndex = this.carros.length - 1;
+    } else {
+      this.carroAtualIndex--;
+    }
+  }
 }
