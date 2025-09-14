0cfe8b828d2fbd05464ad93883075fff2e9a8321
Andre Derme <andredermee@gmail.com>
2025-09-13 23:36:21 -0300

    Create carros.ts

---
+++ TCC/src/app/pages/carros/carros.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carros',
+  imports: [],
+  templateUrl: './carros.html',
+  styleUrl: './carros.css'
+})
+export class Carros {
+
+}
