93bf71e47c7f4f1a762a0a2b69551db012d3c8d4
Andre Derme <andredermee@gmail.com>
2025-09-19 11:33:38 -0300

    Create model-focus.ts

---
+++ TCC/src/app/pages/model-focus/model-focus.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-focus',
+  imports: [],
+  templateUrl: './model-focus.html',
+  styleUrl: './model-focus.css'
+})
+export class ModelFocus {
+
+}
