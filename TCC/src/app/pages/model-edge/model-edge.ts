96d71baefbafb7c992e4ba8b60b4bd525ce612f3
Andre Derme <andredermee@gmail.com>
2025-09-26 18:44:09 -0300

    Create model-edge.ts

---
+++ TCC/src/app/pages/model-edge/model-edge.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-edge',
+  imports: [],
+  templateUrl: './model-edge.html',
+  styleUrl: './model-edge.css'
+})
+export class ModelEdge {
+
+}
