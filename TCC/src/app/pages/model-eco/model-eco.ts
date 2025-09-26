08938d35854ecedaf91a6c108845ba43d7dccbe2
Andre Derme <andredermee@gmail.com>
2025-09-26 19:43:34 -0300

    Update model-eco.ts

--- TCC/src/app/pages/model-eco/model-eco.ts
+++ TCC/src/app/pages/model-eco/model-eco.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../base/header/header";
 
 @Component({
   selector: 'app-model-eco',
-  imports: [],
+  imports: [Header],
   templateUrl: './model-eco.html',
   styleUrl: './model-eco.css'
 })
