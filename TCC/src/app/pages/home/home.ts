6d6406cb9ab4472297cf519a668d8a0a6694d80c
Andre Derme <andredermee@gmail.com>
2025-09-26 19:11:03 -0300

    Update home.ts

--- TCC/src/app/pages/home/home.ts
+++ TCC/src/app/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { Header } from "../base/header/header";
+import { Footer } from "../base/footer/footer"; 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule, Header, Footer], 
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
