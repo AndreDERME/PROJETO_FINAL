e5bd221b1a0549d2adb9747fe86f46e9c9a990bb
Andre Derme <andredermee@gmail.com>
2025-09-26 19:36:25 -0300

    Update ford150-xl.ts

--- TCC/src/app/pages/ford150-xl/ford150-xl.ts
+++ TCC/src/app/pages/ford150-xl/ford150-xl.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../base/header/header";
 
 @Component({
   selector: 'app-ford150-xl',
-  imports: [],
+  imports: [Header],
   templateUrl: './ford150-xl.html',
   styleUrl: './ford150-xl.css'
 })
