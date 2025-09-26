d2966d9add0384a50a250304f313df8b73b5baa2
Andre Derme <andredermee@gmail.com>
2025-09-26 18:44:33 -0300

    Create ford150-xl.ts

---
+++ TCC/src/app/pages/ford150-xl/ford150-xl.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ford150-xl',
+  imports: [],
+  templateUrl: './ford150-xl.html',
+  styleUrl: './ford150-xl.css'
+})
+export class Ford150XL {
+
+}
