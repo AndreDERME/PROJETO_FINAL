42b372e8ae20a018695bde75084c408df301eb3a
Andre Derme <andredermee@gmail.com>
2025-09-26 11:50:40 -0300

    Create model-gt.ts

---
+++ TCC/src/app/pages/model-gt/model-gt.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-gt',
+  imports: [],
+  templateUrl: './model-gt.html',
+  styleUrl: './model-gt.css'
+})
+export class ModelGt {
+
+}
