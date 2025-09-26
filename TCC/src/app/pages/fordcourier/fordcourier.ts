a511f687bd3749d5d068cf1169784fe1c7db5a6d
Andre Derme <andredermee@gmail.com>
2025-09-26 18:44:17 -0300

    Create fordcourier.ts

---
+++ TCC/src/app/pages/fordcourier/fordcourier.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fordcourier',
+  imports: [],
+  templateUrl: './fordcourier.html',
+  styleUrl: './fordcourier.css'
+})
+export class Fordcourier {
+
+}
