dfa77e915076aa17e60aa3b6bd1d7e4297de156b
Andre Derme <andredermee@gmail.com>
2025-09-26 11:50:49 -0300

    Update model-fusion.ts

--- TCC/src/app/pages/model-fusion/model-fusion.ts
+++ TCC/src/app/pages/model-fusion/model-fusion.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../base/header/header';
 
 @Component({
   selector: 'app-model-fusion',
-  imports: [],
+  imports: [Header],
   templateUrl: './model-fusion.html',
   styleUrl: './model-fusion.css'
 })
