4506856a8900ca128f82fecc96f4af4cf7648e16
Andre Derme <andredermee@gmail.com>
2025-09-26 18:43:40 -0300

    Create model-taunus.ts

---
+++ TCC/src/app/pages/model-taunus/model-taunus.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-taunus',
+  imports: [],
+  templateUrl: './model-taunus.html',
+  styleUrl: './model-taunus.css'
+})
+export class ModelTaunus {
+
+}
