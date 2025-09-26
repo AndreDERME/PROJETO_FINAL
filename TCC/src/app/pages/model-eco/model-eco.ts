6e09e8cf251a4e8d691f558c3414c8ea584adc49
Andre Derme <andredermee@gmail.com>
2025-09-25 22:01:57 -0300

    Create model-eco.ts

---
+++ TCC/src/app/pages/model-eco/model-eco.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-eco',
+  imports: [],
+  templateUrl: './model-eco.html',
+  styleUrl: './model-eco.css'
+})
+export class ModelEco {
+
+}
