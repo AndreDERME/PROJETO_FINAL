96f68231fbf14149910721fd0b7bd94b338d9dd0
Andre Derme <andredermee@gmail.com>
2025-09-26 18:44:24 -0300

    Create ford150.ts

---
+++ TCC/src/app/pages/ford150/ford150.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ford150',
+  imports: [],
+  templateUrl: './ford150.html',
+  styleUrl: './ford150.css'
+})
+export class Ford150 {
+
+}
