2e83b6c9d4252637d81b56f04999d170e9b5a642
Andre Derme <andredermee@gmail.com>
2025-09-26 10:18:13 -0300

    Create model-ford-ka.ts

---
+++ TCC/src/app/pages/model-ford-ka/model-ford-ka.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-ford-ka',
+  imports: [],
+  templateUrl: './model-ford-ka.html',
+  styleUrl: './model-ford-ka.css'
+})
+export class ModelFordKa {
+
+}
