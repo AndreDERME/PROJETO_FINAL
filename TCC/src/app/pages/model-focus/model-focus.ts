6034fcdf5e9d7856e15644844b5bb384919d0883
Andre Derme <andredermee@gmail.com>
2025-09-26 19:43:20 -0300

    Update model-focus.ts

--- TCC/src/app/pages/model-focus/model-focus.ts
+++ TCC/src/app/pages/model-focus/model-focus.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../base/header/header";
 
 @Component({
   selector: 'app-model-focus',
-  imports: [],
+  imports: [Header],
   templateUrl: './model-focus.html',
   styleUrl: './model-focus.css'
 })
