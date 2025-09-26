75287f830c0b220a35a8eff137c5b67480d04e84
Andre Derme <andredermee@gmail.com>
2025-09-25 22:01:39 -0300

    Create model-feista.ts

---
+++ TCC/src/app/pages/model-feista/model-feista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-feista',
+  imports: [],
+  templateUrl: './model-feista.html',
+  styleUrl: './model-feista.css'
+})
+export class ModelFeista {
+
+}
