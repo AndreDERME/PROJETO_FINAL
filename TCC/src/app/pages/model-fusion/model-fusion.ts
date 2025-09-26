7429c084a8590a3b57f2e020649356d25681e325
Andre Derme <andredermee@gmail.com>
2025-09-26 10:18:06 -0300

    Create model-fusion.ts

---
+++ TCC/src/app/pages/model-fusion/model-fusion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-fusion',
+  imports: [],
+  templateUrl: './model-fusion.html',
+  styleUrl: './model-fusion.css'
+})
+export class ModelFusion {
+
+}
