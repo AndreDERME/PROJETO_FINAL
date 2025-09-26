0ff5b7ea0322b7c830295df2b7a8073f24d71dd7
Andre Derme <andredermee@gmail.com>
2025-09-26 18:43:48 -0300

    Create model-mustang.ts

---
+++ TCC/src/app/pages/model-mustang/model-mustang.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-mustang',
+  imports: [],
+  templateUrl: './model-mustang.html',
+  styleUrl: './model-mustang.css'
+})
+export class ModelMustang {
+
+}
