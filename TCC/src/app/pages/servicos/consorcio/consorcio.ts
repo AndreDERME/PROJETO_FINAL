3f2e8489ff50efa27dbac0a8a9b7206ba8029c86
Andre Derme <andredermee@gmail.com>
2025-09-26 23:28:57 -0300

    Create consorcio.ts

---
+++ TCC/src/app/pages/servicos/consorcio/consorcio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consorcio',
+  imports: [],
+  templateUrl: './consorcio.html',
+  styleUrl: './consorcio.css'
+})
+export class Consorcio {
+
+}
