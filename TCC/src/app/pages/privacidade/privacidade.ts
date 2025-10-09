1be7747b7a2cd0d600d1c82cc6901587513ce679
Andre Derme <andredermee@gmail.com>
2025-10-08 22:04:52 -0300

    Update privacidade.ts

--- TCC/src/app/pages/privacidade/privacidade.ts
+++ TCC/src/app/pages/privacidade/privacidade.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Header } from '../base/header/header';
 
 @Component({
   selector: 'app-privacidade',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, Header],
   templateUrl: './privacidade.html',
   styleUrl: './privacidade.css'
 })
 export class Privacidade {
-
+  constructor() {
+    window.scrollTo(0, 0);
+  }
 }
