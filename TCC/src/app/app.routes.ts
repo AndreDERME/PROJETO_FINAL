289064778d9ec53b2cfef3350c1e3856c5c6f515
Andre Derme <andredermee@gmail.com>
2025-11-04 21:26:41 -0300

    Update app.routes.ts

--- TCC/src/app/app.routes.ts
+++ TCC/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
-import { LoginComponent } from './pages/login/login'; // ALTERADO: O nome correto do componente é LoginComponent
+
 import { Carros } from './pages/carros/carros';
 import { Contato } from './pages/contato/contato';
 import { Servicos } from './pages/servicos/servicos';
@@ -27,10 +27,6 @@ export const routes: Routes = [
 
     },
 
-        {
-        path: "login",
-        component: LoginComponent, 
-    },
 
        {
         path: "carros",
